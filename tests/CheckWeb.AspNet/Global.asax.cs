c7235a7936a45b249557c29b60cc71700790407c
李兰军 <daves2009@126.com>
2018-03-22 09:08:34 +0800

    add verification code apply in asp.net

---
+++ tests/CheckWeb.AspNet/Global.asax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using Funq;
+using ServiceStack;
+using YeeTech.VerificationCode;
+using YeeTech.VerificationCode.Image;
+using YeeTech.VerificationCode.Interface;
+
+namespace CheckWeb.AspNet
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("Verification Code Service", Assembly.GetExecutingAssembly())
+        {
+        }
+
+        public override void Configure(Container container)
+        {
+            container.Register<IVerificationCode>(new GeneralVerificationCode());
+            container.RegisterAs<ImageVerificationCodeProvider, IImageVerificationCodeFactory>()
+                .InitializedBy((c, f) =>
+                {
+                    f.Width = 80;
+                    f.Height = 32;
+                    f.ImageDrawCompletedHandler = text =>
+                    {
+                        // store in session
+                    };
+                })
+                .ReusedWithin(ReuseScope.Container);
+        }
+    }
+}
---
+++ tests/CheckWeb.AspNet/VerificationCodeService.cs
@@ -0,0 +1,22 @@
+﻿using ServiceStack;
+using YeeTech.VerificationCode.Interface;
+
+namespace CheckWeb.AspNet
+{
+    public class VerificationCodeService : Service
+    {
+        public IImageVerificationCodeFactory Factory { get; set; }
+
+        public object Get(GetVcodeRequest request)
+        {
+            var bytes = Factory.Draw();
+
+            return new HttpResult(bytes, MimeTypes.ImageJpg);
+        }
+    }
+
+    [Route("/vcode")]
+    public class GetVcodeRequest
+    {
+    }
+}
