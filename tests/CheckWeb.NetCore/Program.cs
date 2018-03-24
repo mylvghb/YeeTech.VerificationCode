320ee04e6ec02370e8c1b4ca5eb5265f1cd14787
daves2009 <daves2009@126.com>
2018-03-24 21:44:01 +0800

    remove unused references

--- tests/CheckWeb.NetCore/Program.cs
+++ tests/CheckWeb.NetCore/Program.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using Microsoft.Extensions.Logging;
 
 namespace CheckWeb.NetCore
 {
@@ -18,9 +10,11 @@ namespace CheckWeb.NetCore
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+        }
     }
 }
